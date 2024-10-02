d788e535fbf53e36766a522f19dc368322c97281
safayetneyam <onyttric@gmail.com>
2024-10-03 01:10:15 +0600

    Demo Ticketing Project | Seat Selection & Pricing

---
+++ extras/A05-B09/tailwind-config.js
@@ -0,0 +1,21 @@
+const tailwindConfig = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#1dd100',
+                secondary: '#030712',
+                couponOne: '#ffbf0f',
+                couponTwo: '#f78c9c',
+                priceMenu: '#f7f8f8',
+                body: '#03071299',
+                buttonColor: 'rgba(29, 209, 0, 0.15)',
+            },
+            fontFamily: {
+                RaleWay: 'Raleway',
+                Inter: 'Inter',
+            },
+        },
+    },
+};
+
+tailwind.config = tailwindConfig;
